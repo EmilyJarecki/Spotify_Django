db1369a32a1208209a38ead97e99f2fee7e4b073
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-26 20:56:55 -0600

    Display related Songs on Artist Detail View. Change time format

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import time
 
 # Create your models here.
 class Artist(models.Model):
@@ -16,3 +17,18 @@ class Artist(models.Model):
     # https://docs.djangoproject.com/en/4.1/ref/models/options/
     class Meta:
         ordering = ['name']
+
+# below Artist Model
+
+class Song(models.Model):
+
+    title = models.CharField(max_length=150)
+    length = models.IntegerField(default=0)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="songs")
+
+    def __str__(self):
+        return self.title
+        
+    # Here we define the method to look at the length property and convert it
+    def get_length(self):
+        return time.strftime("%-M:%S", time.gmtime(self.length))
