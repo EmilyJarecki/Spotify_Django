f7462b0e9ba9e0598c3de0b20a15f12ba96d8eaa
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-26 20:33:44 -0600

    full CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    # empty string is localhost
+    # '' => localhost:8000
+    # views.Home => views.py => class Home
+    # name: Helpful for navigation, we can simply say the success url (redirect) is the path named "home"
     path('', views.Home.as_view(), name="home"),
-    # pay attention that there is no slash before the word
     path('about/', views.About.as_view(), name="about"),
-    path('artists/', views.ArtistList.as_view(), name = 'artist_list'),
-    path('songs/', views.SongList.as_view(), name='song_list'),
+    path('artists/', views.ArtistList.as_view(), name="artist_list"),
+    path('songs/', views.SongList.as_view(), name="song_list"),
+    # Creating an Artist
+    path('artists/new/', views.ArtistCreate.as_view(), name="artist_create"),
+    # Show urlpattern
+    path('artists/<int:pk>', views.ArtistDetail.as_view(), name="artist_detail"),
+    # Update urlpattern
+    path('artists/<int:pk>/update', views.ArtistUpdate.as_view(), name="artist_update"),
+    # Delete urlpattern
+    path('artists/<int:pk>/delete', views.ArtistDelete.as_view(), name="artist_delete")
 ]
-
